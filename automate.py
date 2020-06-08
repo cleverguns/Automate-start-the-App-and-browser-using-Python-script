1577dc717108be0c87792cd771cb94f43b4460ca
CaLi <41587193+cleverguns@users.noreply.github.com>
2020-06-08 13:26:58 +0800

    Add files via upload

---
+++ automate.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+webbrowser.open('https://remotedesktop.google.com/access/')  
+# To automate open the google remote
+
+
+import os
+
+os.startfile('C:\\Program Files (x86)\\Radmin Viewer 3\\Radmin.exe') 
+
+#open exe files
+  
