3d93e467d38248be60e8d2cdf8a40c0eb50c7e43
CaLi <41587193+cleverguns@users.noreply.github.com>
2020-06-11 11:55:08 +0800

    Add files via upload

--- automate.py
+++ automate.py
@@ -8,5 +8,7 @@ import os
 
 os.startfile('C:\\Program Files (x86)\\Radmin Viewer 3\\Radmin.exe') 
 
+
+os.startfile('C:\\Program Files (x86)\\Advanced IP Scanner\\advanced_ip_scanner.exe')
 #open exe files
   
